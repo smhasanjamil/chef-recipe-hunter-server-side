e432af3e1a7851288606d4910f01687365b9cf40
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-04 10:22:44 +0600

    Load json data and get allData api link

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const allChef = require("./data.json");
+
 app.get('/', (req, res) => {
     res.json({ message: "My server is running" });
 });
 
 app.get('/allData', (req, res) => {
-    // res.send(allServices)
+    res.send(allChef)
     // console.log('Hello')
 });
 
