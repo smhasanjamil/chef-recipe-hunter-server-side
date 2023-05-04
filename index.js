4a62546ebc0cbc69e91803e67dbf8543de2537ff
smhasanjamil <smhasanjamil23@gmail.com>
2023-05-04 13:16:58 +0600

    Find data with params id

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.get('/allData', (req, res) => {
     // console.log('Hello')
 });
 
+app.get('/allData/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = allChef?.find(uid=>uid.id==id);
+    res.send({chef});
+    // console.log(chef);
+});
+
 app.listen(5000, () => {
     console.log('Server running on the port 5000');
 })
